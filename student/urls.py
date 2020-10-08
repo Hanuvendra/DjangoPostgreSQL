7d706a8bb4f4eabd8a2ffa56d644865724164737
Hanuvendra Pandey <hanuvendrap2014@gmail.com>
2020-10-08 16:50:34 +0530

    CRUD on student data

---
+++ student/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+from .views import StudentViewSet
+
+router = SimpleRouter()
+router.register('student', StudentViewSet)
+urlpatterns = router.urls
